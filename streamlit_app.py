64efd101e23419d103580474f9bce454d6f41ef0
streamlitapp2708 <dhall.munish@gmail.com>
2024-12-23 16:09:39 +0000

    Page_database fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ project_2_page = st.Page(
     icon=":material/smart_toy:",
 )
 project_3_page = st.Page(
-    "views/Page_database.py",
+    "views/Page_Database.py",
     title="Database",
     icon=":material/smart_toy:",
 )
